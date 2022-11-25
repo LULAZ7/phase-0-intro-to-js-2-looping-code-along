dec0f1b14a5b4eb8762408c5f2ea1c4452d673a4
Lula <loulazakaria02@gmail.com>
2022-11-25 21:34:36 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Code your solutions in this file
+let names=['Guadalupe','Ollie','Aki'];
+function writeCards(names){
+    let messages=[];
+    let message;
+    for(let i=0; i<names.length; i++){
+        message= 'Thank you, ${names[i]},for the womderful surprise gift!'
+        console.log(message);
+        messages.push(message);
+        debugger;
+    }
+    return messages;
+}
+
+writeCards(names);
+
+
+function countDown(number) {
+    let i = 0;
+    let newNum= number;
+    while(i<=number){
+        console.log(newNum);
+        i++
+        newNum-+1;
+    }
+}
+
+countDown(5);
